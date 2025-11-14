58a1924bbfb6c660cb5f7f09428b3da8639eb1b8
laviniapark <laviniasoo03@gmail.com>
2025-11-14 20:45:03 -0300

    Migrations atualizadas

---
+++ AiManagementApp/Migrations/20251114234013_UpdateTableName.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AiManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AiLogs",
+                table: "AiLogs");
+
+            migrationBuilder.RenameTable(
+                name: "AiLogs",
+                newName: "TBL_AI_LOGS");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TBL_AI_LOGS",
+                table: "TBL_AI_LOGS",
+                column: "log_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TBL_AI_LOGS",
+                table: "TBL_AI_LOGS");
+
+            migrationBuilder.RenameTable(
+                name: "TBL_AI_LOGS",
+                newName: "AiLogs");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AiLogs",
+                table: "AiLogs",
+                column: "log_id");
+        }
+    }
+}
